3ae97d53a76a0165e867682deb31c17ee729b4a0
DaXuanGuJi <541608265@qq.com>
2025-08-20 01:45:06 +0800

    Add files armbian upload

--- sendUrl/sendUrl.js
+++ sendUrl/sendUrl.js
@@ -1,1 +1,1 @@
-var sendExeUrl="http://[2408:8956:50:7504:7603:b1e4:ea26:6207]/_github/book_file_server/";
+var sendExeUrl="http://[2408:8956:5010:78b3:76e1:af47:98e5:e836]/_github/book_file_server/";
